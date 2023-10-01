648c5defe1f9d6080ec765b913d317724d0d91d5
Cauê Melo <cauesmelo@gmail.com>
2023-10-01 08:08:41 -0300

    feat: basic storm mechanic

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -4,39 +4,50 @@ extends CharacterBody2D
 
 var air_jump = false
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-var Colors = preload("res://scripts/colors.gd").names
 
 var is_dead = false
 var wait_death = 0.2
 var wait_respawn = 1.5
+var is_storming = false
+var storm_from
+var storm_to
+var storm_timer = 0
 
 @onready var world = get_tree().get_root().get_child(1)
 @onready var global_cam = world.get_node("global_cam")
-@onready var animated_sprite_2d = $sprite
+@onready var animated_sprite_2d = $sprite_white
 @onready var coyote_jump_timer = $CoyoteJumpTimer
 @onready var starting_position = global_position
+@onready var sprites = [$sprite_white, $sprite_green]
 
-var color = Colors.WHITE
+var color = "white"
 
 func _physics_process(delta):
 	
 	if is_dead:
 		handle_death(delta)
 		return
-	
+		
 	apply_gravity(delta)
-	handle_jump()
+	
+	if not is_storming:
+		handle_jump()
+		
 	var input_axis = Input.get_axis("mv_left", "mv_right")
 	handle_acceleration(input_axis, delta)
 	handle_air_acceleration(input_axis, delta)
+	handle_switch_color()
 	apply_friction(input_axis, delta)
 	apply_air_resistance(input_axis, delta)
 	var was_on_floor = is_on_floor()
 	move_and_slide()
 	var just_left_ledge = was_on_floor and not is_on_floor() and velocity.y >= 0
+	
 	if just_left_ledge:
 		coyote_jump_timer.start()
-	update_animations(input_axis)
+	
+	if not is_storming:	
+		update_animations(input_axis)
 	
 	if position.y > 300:
 		die()
@@ -76,7 +87,10 @@ func apply_air_resistance(input_axis, delta):
 
 func update_animations(input_axis):
 	if input_axis != 0:
-		animated_sprite_2d.flip_h = (input_axis < 0)
+		
+		for sprite in sprites:
+			sprite.flip_h = (input_axis < 0)	
+		
 		animated_sprite_2d.play("run")
 	else:
 		animated_sprite_2d.play("idle")
@@ -103,23 +117,81 @@ func handle_death(delta):
 
 func die():
 	is_dead = true
+	
+	if is_storming:
+		animated_sprite_2d.visible = false
+		
+		if storm_to == "green":
+			animated_sprite_2d = $sprite_green
+		
+		if storm_to == "white":
+			animated_sprite_2d = $sprite_white
+		
+		animated_sprite_2d.visible = true
+		is_storming = false
+		
 	$collision.call_deferred("set_disabled", true)
 	velocity.y = movement_data.jump_velocity
-	$sprite.stop()
+	animated_sprite_2d.stop()
 	$cam.enabled = false
 	global_cam.position = position
 	global_cam.enabled = true
-	print("matou")
-	print(get_stack())
+
 
 func respawn():
 	var pos = get_parent().get_node("respawn").position
 	position = pos
 	is_dead = false
 	$collision.disabled = false
-	$sprite.play("idle")
+	animated_sprite_2d.play("idle")
 	$cam.enabled = true
 	global_cam.enabled = false
 	velocity.y = 0
 	wait_respawn = 1.5
 	wait_death = 0.2
+	
+func handle_switch_color():
+	if Input.is_action_just_pressed("mv_action"):
+		if color == "white":
+			switch_color("green")
+		elif color == "green":
+			switch_color("white")
+			
+	
+func switch_color(color_to):
+	is_storming = true
+	storm_from = color
+	storm_to = color_to
+		
+	animated_sprite_2d.play("storm_in")
+
+	color = color_to
+	$color.text = color_to
+	
+	
+func _on_sprite_animation_finished():
+	if is_dead:
+		return
+		
+	if is_storming:
+		var curr = animated_sprite_2d.get_animation()
+		
+		if curr == "storm_in":
+			animated_sprite_2d.play("storm")
+			
+		if curr == "storm":
+			animated_sprite_2d.visible = false
+				
+			if storm_to == "green":
+				animated_sprite_2d = $sprite_green
+			
+			if storm_to == "white":
+				animated_sprite_2d = $sprite_white
+				
+				
+			animated_sprite_2d.visible = true
+			animated_sprite_2d.play("storm_out")
+				
+		if curr == "storm_out":
+			is_storming = false
+			animated_sprite_2d.play("idle")
